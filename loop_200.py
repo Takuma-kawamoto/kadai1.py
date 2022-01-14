6c0760a07fbdd1813e2df833c0ffe0769589b6b6
川本　拓真 <97686140+Takuma-kawamoto@users.noreply.github.com>
2022-01-14 19:12:07 +0900

    Create loop_200.py
    100回ループするコードを100回から200回に変換しました。

---
+++ loop_200.py
@@ -0,0 +1,9 @@
+for i in range(201):
+    if   i%15 == 0:
+        print("大学爆破したい")
+    elif i%5 == 0:
+        print("就活したい")
+    elif i%3 == 0:
+        print("勉強したい")
+    else:
+        print(i)
