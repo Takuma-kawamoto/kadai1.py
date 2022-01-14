570c97f556888f9de21603befeb6967b1969091d
川本　拓真 <97686140+Takuma-kawamoto@users.noreply.github.com>
2022-01-14 19:02:54 +0900

    Update and rename README.md to kadai1.py

---
+++ kadai1.py
@@ -0,0 +1,10 @@
+for i in range(101):
+    if   i%15 == 0:
+        print("大学爆破したい")
+    elif i%5 == 0:
+        print("就活したい")
+    elif i%3 == 0:
+        print("勉強したい")
+    else:
+        print(i)
+
